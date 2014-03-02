792d56ac698b6727de4df0b2a028d4dcf1dabe13
Shoichi Matsuda <marcos.shoichi.matsuda@gmail.com>
2014-03-02 14:37:16 +0900

    initial commit

---
+++ MockTestSample/MTSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MTSAppDelegate.h
+//  MockTestSample
+//
+//  Created by Shoichi Matsuda on 2014/03/02.
+//  Copyright (c) 2014年 Shoichi Matsuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MTSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MockTestSample/MTSViewController.h
@@ -0,0 +1,13 @@
+//
+//  MTSViewController.h
+//  MockTestSample
+//
+//  Created by Shoichi Matsuda on 2014/03/02.
+//  Copyright (c) 2014年 Shoichi Matsuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MTSViewController : UIViewController
+
+@end
